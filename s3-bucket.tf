aee4b239f628aad8e18034170393c52b4113f60b
gvk789 <gvk789@gmail.com>
2021-05-14 20:36:27 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,22 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/KrishnaG-training/s3-bucket/aws"
+  version = "1.15.0"
+  # insert required variables here
+//--------------------------------------------------------------------
+// Variables
+variable "s3_bucket_policy" {}
+
+//--------------------------------------------------------------------
+// Modules
+module "s3_bucket" {
+  source  = "app.terraform.io/KrishnaG-training/s3-bucket/aws"
+  version = "2.1.0"
+
+  acceleration_status = "Suspended"
+  bucket = "Training-S3"
+  bucket_prefix = "KrishnaG-"
+  policy = "${var.s3_bucket_policy}"
+  request_payer = "devops"
+  tags = "Billable = "true" ; Department = "devops""
+}
+}
